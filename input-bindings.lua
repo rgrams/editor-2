50cc7830504e893f487f1e9d3dd01399d6191213
Ross Grams <ross.grams@gmail.com>
2022-04-05 11:03:35 -0400

    Add boilerplate main, conf, input-bindings

---
+++ input-bindings.lua
@@ -0,0 +1,28 @@
+
+return {
+	{ "cursor", "mouse moved" },
+	{ "button", "M:1", "click" },
+	{ "button", "M:3", "pan camera" },
+	{ "button", "M:wheely+", "zoom camera" },
+	{ "button", "M:wheely-", "zoom camera" },
+
+	{ "button", "up", "up" },
+	{ "button", "down", "down" },
+	{ "button", "left", "left" },
+	{ "button", "right", "right" },
+
+	{ "button", "tab", "next" },
+	{ "button", "lshift", "shift" },
+	{ "button", "rshift", "shift" },
+
+	{ "button", "return", "enter" },
+	{ "button", "kpenter", "enter" },
+	{ "text", "text" },
+	{ "button", "backspace", "backspace" },
+	{ "button", "delete", "delete" },
+	{ "button", "end", "end" },
+	{ "button", "home", "home" },
+	{ "button", "escape", "cancel" },
+	{ "button", "lshift", "selection modifier" },
+	{ "button", "rshift", "selection modifier" },
+}
---
+++ main.lua
@@ -0,0 +1,54 @@
+
+require "philtre.init"
+_G.gui = require "philtre.objects.gui.all"
+
+local scene
+local ui
+
+local screenRect = gui.Rect(0, 0, love.graphics.getDimensions())
+
+local layers = {
+	world = { "default" },
+	gui = { "gui text", "gui" },
+	guiDebug = { "guiDebug" },
+}
+local defaultLayer = "default"
+
+local guiDebugDrawEnabled = false
+
+function love.load()
+	math.randomseed(love.timer.getTime() * 10000)
+	math.random()  math.random()  math.random()
+
+	Input.init()
+	Input.bind( require("input-bindings") )
+	love.keyboard.setKeyRepeat(true)
+
+	scene = SceneTree(layers, defaultLayer)
+
+	-- ui = scene:add( require("interface.UI")() )
+end
+
+function love.update(dt)
+	scene:update(dt)
+end
+
+function love.draw()
+	scene:updateTransforms()
+	Camera.current:applyTransform()
+	scene:draw("world")
+	Camera.current:resetTransform()
+	scene:draw("gui")
+
+	if guiDebugDrawEnabled then
+		ui:callRecursive("debugDraw", "guiDebug")
+		scene:draw("guiDebug")
+		scene.draw_order:clear("guiDebug")
+	end
+end
+
+function love.resize(w, h)
+	Camera.setAllViewports(0, 0, w, h)
+	screenRect.w, screenRect.h = w, h
+	-- ui:allocate(screenRect)
+end
