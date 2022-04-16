509d567a1d9f2133b063ee15aa54ada19fd80966
Ross Grams <ross.grams@gmail.com>
2022-04-15 21:19:00 -0400

    Tool: Add basic box-select

--- tools/Tool.lua
+++ tools/Tool.lua
@@ -2,6 +2,7 @@
 local Tool = gui.Node:extend()
 Tool.className = "Tool"
 
+local config = require "config"
 local scenes = require "scenes"
 local EditorObject = require "objects.EditorObject"
 local objectFn = require "commands.functions.object-functions"
@@ -28,9 +29,9 @@ function Tool.updatePropertiesPanel(self)
 	self.propertyPanel:updateProperties(enclosures)
 end
 
-local function startDrag(self)
+local function startDrag(self, dragType)
 	self.isDragging = true
-	self.ruu:startDrag(self.widget)
+	self.ruu:startDrag(self.widget, dragType)
 
 	local wmx, wmy = Camera.current:screenToWorld(self.ruu.mx, self.ruu.my)
 	self.lastDragX, self.lastDragY = wmx, wmy
@@ -39,11 +40,12 @@ end
 
 local function stopDrag(self)
 	self.isDragging = false
+	self.isBoxSelecting = false
 	self.ruu:stopDraggingWidget(self.widget)
 	self.startedDragCommand = false
 end
 
-function Tool.drag(wgt, dx, dy)
+function Tool.drag(wgt, dx, dy, dragType)
 	local self = wgt.object
 	local scene = scenes.active
 
@@ -51,18 +53,39 @@ function Tool.drag(wgt, dx, dy)
 	local wdx, wdy = x - self.lastDragX, y - self.lastDragY
 	self.lastDragX, self.lastDragY = x, y
 
-	if not self.startedDragCommand then
-		self.startedDragCommand = true
-		local enclosures = scene.selection:copyList()
-		scene.history:perform("offsetPropertyOnMultiple", enclosures, "pos", wdx, wdy)
-		self:updatePropertiesPanel()
-	else
-		-- TODO: Make sure the last command in the history is still ours.
-		local enclosures = scene.selection:copyList()
-		objectFn.offsetPropertyOnMultiple(enclosures, "pos", wdx, wdy)
-		local totalDX, totalDY = x - self.dragStartX, y - self.dragStartY
-		scene.history:update(enclosures, "pos", totalDX, totalDY)
-		self:updatePropertiesPanel()
+	if dragType == "drag selection" then
+		if not self.startedDragCommand then
+			self.startedDragCommand = true
+			local enclosures = scene.selection:copyList()
+			scene.history:perform("offsetPropertyOnMultiple", enclosures, "pos", wdx, wdy)
+			self:updatePropertiesPanel()
+		else
+			-- TODO: Make sure the last command in the history is still ours.
+			local enclosures = scene.selection:copyList()
+			objectFn.offsetPropertyOnMultiple(enclosures, "pos", wdx, wdy)
+			local totalDX, totalDY = x - self.dragStartX, y - self.dragStartY
+			scene.history:update(enclosures, "pos", totalDX, totalDY)
+			self:updatePropertiesPanel()
+		end
+	elseif dragType == "box select" then
+		self.isBoxSelecting = true
+		local lt, top = math.min(x, self.dragStartX), math.min(y, self.dragStartY)
+		local w, h = math.abs(x - self.dragStartX), math.abs(y - self.dragStartY)
+		local selectedEnclosures = {}
+		for i,child in ipairs(scene.children) do
+			local cx, cy = child:toWorld(0, 0)
+			if cx >= lt and cx <= lt+w and cy >= top and cy <= top+h then
+				table.insert(selectedEnclosures, child.enclosure)
+			end
+		end
+		if not self.startedDragCommand then
+			self.startedDragCommand = true
+			scene.history:perform("setSelection", scene.selection, selectedEnclosures)
+			self:updatePropertiesPanel()
+		else
+			scene.selection:setTo(selectedEnclosures)
+			scene.history:update(scene.selection, selectedEnclosures)
+		end
 	end
 end
 
@@ -96,7 +119,7 @@ function Tool.press(wgt, depth, mx, my, isKeyboard)
 				self:updatePropertiesPanel()
 			end
 			if self.hoverObj.isSelected then
-				startDrag(self)
+				startDrag(self, "drag selection")
 			end
 		else -- Clicked on nothing.
 			local selection = scenes.active.selection
@@ -104,6 +127,7 @@ function Tool.press(wgt, depth, mx, my, isKeyboard)
 				scenes.active.history:perform("clearSelection", selection)
 				self:updatePropertiesPanel()
 			end
+			startDrag(self, "box select")
 		end
 	end
 end
@@ -147,6 +171,18 @@ function Tool.ruuInput(wgt, depth, action, value, change, rawChange, isRepeat, x
 end
 
 function Tool.draw(self)
+	if self.isBoxSelecting then
+		local sx1, sy1 = Camera.current:worldToScreen(self.dragStartX, self.dragStartY)
+		local sx2, sy2 = Camera.current:worldToScreen(self.lastDragX, self.lastDragY)
+		local lx1, ly1 = self:toLocal(sx1, sy1)
+		local lx2, ly2 = self:toLocal(sx2, sy2)
+		local sw, sh = lx2 - lx1, ly2 - ly1
+		local col = config.selectedHighlightColor
+		love.graphics.setColor(col)
+		love.graphics.rectangle("line", lx1, ly1, sw, sh)
+		love.graphics.setColor(col[1], col[2], col[3], 0.02)
+		love.graphics.rectangle("fill", lx1, ly1, sw, sh)
+	end
 end
 
 return Tool
