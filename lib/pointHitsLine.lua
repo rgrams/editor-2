4f4ca79c0d84001fead22f6f43a817a468555be7
Ross Grams <ross.grams@gmail.com>
2022-06-10 14:36:20 -0400

    Polygon Tool: Fix edge collision - forgot to clamp fraction to 0-1

--- lib/pointHitsLine.lua
+++ lib/pointHitsLine.lua
@@ -7,6 +7,7 @@ local function pointHitsLine(px, py, x1, y1, x2, y2, lineRadius)
 	local x1_to_px, y1_to_py = px - x1, py - y1 -- Vector from line start to point.
 	local distAlongLine = vec2.dot(x1_to_px, y1_to_py, dx, dy)
 	local fractionAlongLine = distAlongLine / len2 -- Divide by len^2 since dx,dy are not normalized?
+	fractionAlongLine = math.max(0, math.min(fractionAlongLine, 1))
 	local closestX, closestY = vec2.lerp(x1, y1, x2, y2, fractionAlongLine)
 	local dist = vec2.len(px - closestX, py - closestY)
 	if dist <= lineRadius then
