4eb710f9bd1bcfb08bab8650d85f6c3f83118d84
rgrams <ross.grams@gmail.com>
2022-07-15 10:52:45 -0400

    Reduce alpha of parent lines & axes, + other visual tweaks - Make parent lines -slightly- shorter with a larger arrow. - Also remove unused color settings from config.

--- config.lua
+++ config.lua
@@ -34,19 +34,16 @@ M.viewportBackgroundColor = { 0.1, 0.1, 0.1 }
 M.gridColor = { 0.3, 0.3, 0.3, 0.06 }
 M.bigGridColor = { 0.3, 0.3, 0.3, 0.28 }
 M.gridNumberColor = { 0.5, 0.5, 0.5, 0.5 }
-M.xAxisColor = { 0.8, 0.4, 0.4, 0.4 }
-M.yAxisColor = { 0.4, 0.8, 0.4, 0.4 }
+M.xAxisColor = { 0.8, 0.4, 0.4, 0.2 }
+M.yAxisColor = { 0.4, 0.8, 0.4, 0.2 }
 
--- M.hoverHighlightColor = {1, 0.9, 0.8, 0.4}
-M.hoverHighlightColor = {1, 0.9, 0.8, 1}
 M.selectedHighlightColor = {0.9, 0.5, 0.0, 0.9}
-M.latestSelectedHighlightColor = {1, 0.9, 0.45, 1}
 M.highlightLineWidth = 2
 M.highlightPadding = -1 -- Half of line width, so outer edge of line matches bounds.
 
-M.parentLineColor = {0, 1, 1, 0.3}
-M.parentLineLenFrac = 0.95
+M.parentLineColor = {0, 1, 1, 0.05}
+M.parentLineLenFrac = 0.92
 M.parentLineArrowAngle = 0.4
-M.parentLineArrowLength = 20
+M.parentLineArrowLength = 25
 
 return M
